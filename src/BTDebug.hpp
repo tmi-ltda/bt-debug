813f8bf90ea0512da9b78e94691db7cdf01032db
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-26 08:03:27 -0300

    inicialização da biblioteca

---
+++ src/BTDebug.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+
+class BTDebug : public Print {
+  private:
+    HardwareSerial& _primary;
+    BluetoothSerial& _secondary;
+
+  public:
+    BTDebug(HardwareSerial& p, BluetoothSerial& s) : _primary(p), _secondary(s) {}
+
+    void begin(unsigned long baud) {
+      _primary.begin(baud);
+      _secondary.begin(baud);
+    }
+
+    size_t write(uint8_t c) override {
+      _primary.write(c);
+      _secondary.write(c);
+      return 1;
+    }
+
+    using Print::write;
+};
