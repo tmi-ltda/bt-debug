f4ed8cc15b64cb31c18b72e0f37eaf0e725e16c1
Paulo Prates <paulo.prates01@hotmail.com>
2025-05-28 09:18:22 -0300

    refactor: classe reescrita para herdar de Print

--- src/BTDebug.hpp
+++ src/BTDebug.hpp
@@ -3,24 +3,50 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 
-class BTDebug : public HardwareSerial {
+class BTDebug : public Print {
   private:
+    HardwareSerial& _serial;
     BluetoothSerial& _bt;
 
   public:
-    BTDebug(int uart_num, BluetoothSerial& bt) : HardwareSerial(uart_num), _bt(bt) { }
+    BTDebug(HardwareSerial& serial, BluetoothSerial& bt) 
+      : _serial(serial), _bt(bt) { }
+
+    void begin(unsigned long baudrate) {
+      _serial.begin(baudrate);
+    }
+
+    void btBegin(const char* name) {
+      _bt.begin(name);
+    }
 
     size_t write(uint8_t c) override {
+      _serial.write(c);
       _bt.write(c);
-
-      return HardwareSerial::write(c);
+      return 1;
     }
 
     size_t write(const uint8_t *buffer, size_t size) override {
-        _bt.write(buffer, size);
-        
-        return HardwareSerial::write(buffer, size);
+      _serial.write(buffer, size);
+      _bt.write(buffer, size);
+      return size;
     }
 
-    using HardwareSerial::write;
+    using Print::write;
+
+    int available() {
+      return _serial.available();
+    }
+
+    int read() {
+      return _serial.read();
+    }
+
+    int peek() {
+      return _serial.peek();
+    }
+
+    void flush() {
+      _serial.flush();
+    }
 };
